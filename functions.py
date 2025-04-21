49858e9f752af748406b896e2a43e4de2961cbea
splendidissimemendax <165426096+splendidissimemendax@users.noreply.github.com>
2025-04-20 22:30:33 -0700

    read in templates

--- functions.py
+++ functions.py
@@ -1,2 +1,23 @@
 def buildSite(): 
 	print("built")
+
+# IMPORT TEMPLATES
+with open("./templates/header.html", "r") as f:
+	headHTML = f.read()
+
+with open("./templates/footer.html", "r") as f:
+	footHTML = f.read()
+
+with open("./templates/page.html", "r") as f:
+	pageHTML = f.read()
+
+# SUB HEADER AND FOOTER
+pageHTML = pageHTML.replace("{{HEADER}}", headHTML)
+pageHTML = pageHTML.replace("{{FOOTER}}", footHTML)
+
+with open("./templates/page.html", "r") as f:
+	postHTML = f.read()
+
+# SUB HEADER AND FOOTER
+postHTML = postHTML.replace("{{HEADER}}", headHTML)
+postHTML = postHTML.replace("{{FOOTER}}", footHTML)
